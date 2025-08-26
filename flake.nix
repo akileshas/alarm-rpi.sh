3d8e9f484a3559edb5f72539c01ddb77c46d7309
Sarguru02 <psgnhealth@gmail.com>
2025-08-26 14:30:35 +0530

    [feat]: add nix flake support

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Install ArchLinux ARM in Raspberry pi 5";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem = {pkgs,...}: {
+        packages.default = pkgs.writeShellApplication {
+          name="alarm_install";
+          runtimeInputs = [pkgs.fzf];
+          text = builtins.readFile ./build.sh;
+        };
+        devShells.default = pkgs.mkShell {
+          name="alarm_install-devshell";
+          meta.description = "Shell environment for alarm_install script";
+          packages = with pkgs;[fzf];
+        };
+      };
+    };
+}
