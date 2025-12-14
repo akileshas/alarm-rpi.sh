55a4ac2db18fd5f3ef33c43eaaec493a332e0d0d
akileshas <akileshas.ai@gmail.com>
2025-12-14 17:06:40 +0530

    feat: add nix flakes

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Install ArchLinux ARM in Raspberry Pi";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      basePkgs = pkgs: with pkgs; [
+        aria2
+        dosfstools
+        e2fsprogs
+        libarchive
+        fzf
+        gawk
+        findutils
+        curl
+        gnugrep
+        coreutils
+        util-linux
+        parted
+      ];
+      forAllSystems = f: builtins.listToAttrs (map (system: {
+        name = system;
+        value = f system nixpkgs.legacyPackages.${system};
+      }) systems);
+    in
+    {
+      packages = forAllSystems (system: pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "alarm-rpi";
+          runtimeInputs = basePkgs pkgs;
+          text = builtins.readFile ./run.bash;
+        };
+      });
+      devShells = forAllSystems (system: pkgs: {
+        default = pkgs.mkShell {
+          name = "alarm-rpi";
+          meta.description = "shell for alarm-rpi installer script";
+          packages = basePkgs pkgs ++ [ self.packages.${system}.default ];
+        };
+      });
+    };
+}
