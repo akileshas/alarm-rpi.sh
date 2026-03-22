32d6e6fbdc60b0c13cb75ff1b61571ff91fcf967
Akilesh A S <io@akileshas.dev>
2026-03-22 14:46:29 +0530

    chore: format

--- flake.nix
+++ flake.nix
@@ -1,45 +1,55 @@
 {
   description = "Install ArchLinux ARM in Raspberry Pi";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      basePkgs = pkgs: with pkgs; [
-        aria2
-        dosfstools
-        e2fsprogs
-        libarchive
-        fzf
-        gawk
-        findutils
-        curl
-        gnugrep
-        coreutils
-        util-linux
-        parted
-      ];
-      forAllSystems = f: builtins.listToAttrs (map (system: {
-        name = system;
-        value = f system nixpkgs.legacyPackages.${system};
-      }) systems);
+      basePkgs =
+        pkgs: with pkgs; [
+          aria2
+          dosfstools
+          e2fsprogs
+          libarchive
+          fzf
+          gawk
+          findutils
+          curl
+          gnugrep
+          coreutils
+          util-linux
+          parted
+        ];
+      forAllSystems =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f system nixpkgs.legacyPackages.${system};
+          }) systems
+        );
     in
     {
-      packages = forAllSystems (system: pkgs: {
-        default = pkgs.writeShellApplication {
-          name = "alarm-rpi";
-          runtimeInputs = basePkgs pkgs;
-          text = builtins.readFile ./run.sh;
-        };
-      });
-      devShells = forAllSystems (system: pkgs: {
-        default = pkgs.mkShell {
-          name = "alarm-rpi";
-          meta.description = "shell for alarm-rpi installer script";
-          packages = basePkgs pkgs ++ [ self.packages.${system}.default ];
-        };
-      });
+      packages = forAllSystems (
+        system: pkgs: {
+          default = pkgs.writeShellApplication {
+            name = "alarm-rpi";
+            runtimeInputs = basePkgs pkgs;
+            text = builtins.readFile ./run.sh;
+          };
+        }
+      );
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            name = "alarm-rpi";
+            meta.description = "shell for alarm-rpi installer script";
+            packages = basePkgs pkgs ++ [ self.packages.${system}.default ];
+          };
+        }
+      );
     };
 }
